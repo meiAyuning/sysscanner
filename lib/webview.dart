ec9ae081e93e7135dc63535e5fe2e724382f715f
meiAyuning <gondrongaselole4@gmail.com>
2022-11-03 09:16:22 +0700

    add center title and remove file newebview.dart

--- lib/webview.dart
+++ lib/webview.dart
@@ -1,7 +1,15 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
+  await Permission.microphone.request();
+
+  runApp(WebViewPage());
+}
 
 class WebViewPage extends StatefulWidget {
   WebViewPage({Key? key}) : super(key: key);
@@ -14,6 +22,11 @@ class _WebViewPageState extends State<WebViewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: const Text('Absensi Siswa'),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
         body: new WebView(
             initialUrl:
                 "https://presensipkl.smkniu.sch.id/mGvwLJfkc4zOJ7As9eEvA==/",

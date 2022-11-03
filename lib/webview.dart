e095f10448b6a11645572dbaba20d9441fe6516c
meiAyuning <gondrongaselole4@gmail.com>
2022-11-03 08:10:20 +0700

    add new file

--- lib/webview.dart
+++ lib/webview.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -12,10 +14,9 @@ class _WebViewPageState extends State<WebViewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new WebView(
-          initialUrl:
-              "https://presensipkl.smkniu.sch.id/mGvwLJfkc4zOJ7As9eEvA==/",
-          javascriptMode: JavascriptMode.unrestricted),
-    );
+        body: new WebView(
+            initialUrl:
+                "https://presensipkl.smkniu.sch.id/mGvwLJfkc4zOJ7As9eEvA==/",
+            javascriptMode: JavascriptMode.unrestricted));
   }
 }
